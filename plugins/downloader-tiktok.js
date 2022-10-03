9f00c7e85e273c43518c93b70f82050147233947
XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>
2022-10-04 06:23:41 +0700

    Update downloader-tiktok.js
    Signed-off-by: XiaoyuOfc <111475019+XiaoyuOfc@users.noreply.github.com>

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,28 +1,21 @@
-import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
-
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
-    const { author: { nickname }, video, description } = await tiktokdl(args[0])
-    .catch(async _ => await tiktokdlv2(args[0]))
-        .catch(async _ => await tiktokdlv3(args[0]))
-    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-    if (!url) throw 'Can\'t download video!'
-    conn.sendFile(m.chat, url, 'tiktok.mp4', 
-`
-⟐⟞⟚⟝⟮ *Usᴇʀɴᴀᴍᴇ:* ⟯⟞⟚⟝⟐
-┇⟣⟪ ${nickname} ⟫⟢
-▥ ━┉┄┄┈┈ ▢
-
-┇⟐⟞⟚⟝⟮ *Dᴇsᴄʀɪᴘᴛɪᴏɴ:* ⟯⟞⟚⟝⟐
-▥ ━┉┄┄┈┈ ▢
-${description}
-◈ ━┉┈┄┈┈ ►
-
-Donasi ngab jan berharap Free mulu:v
-`.trim(), m)
-}
-handler.help = ['tiktok'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(tiktok|tt|ttdl|tiktokdl)$/i
-
-export default handler
+import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper' 
+ import fetch from 'node-fetch' 
+  
+ let handler = async (m, { conn, args, usedPrefix, command }) => { 
+     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243` 
+     const { author: { nickname }, video, description } = await tiktokdl(args[0]) 
+         .catch(async _ => await tiktokdlv2(args[0])) 
+         .catch(async _ => await tiktokdlv3(args[0])) 
+     const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd 
+     if (!url) throw 'Can\'t download video!' 
+     conn.sendHydrated(m.chat, `${htki} ᴛɪᴋᴛᴏᴋ ᴡᴍ ${htka}`, `➔ ɴɪᴄᴋɴᴀᴍᴇ ${nickname}${description ? `\n➔ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ:\n${description}` : ''}`, await (await fetch(url)).buffer(), 
+         url, '🌎 s ᴏ ᴜ ʀ ᴄ ᴇ', null, null, [ 
+         ['ᴅᴏɴᴀꜱɪ', `.donasi`], 
+         ['ᴀᴜᴅɪᴏ', `.tiktokaudio ${args}`], 
+         [null, null]], m) 
+ } 
+ handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>') 
+ handler.tags = ['downloader'] 
+ handler.command = /^(tik(tok)?(tok)?(dl)?)$/i 
+  
+ export default handler
